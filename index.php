1daffa77b71276e65162e4fd71cefeb23e7f0eaa
Adriano Carneiro <adrianodoss@gmail.com>
2022-02-17 17:49:09 -0300

    teste

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
 
-$artigos = [
-    [
-        'titulo' => 'Primeiros passos com Spring',
-        'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-    programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-        'link' => 'primeiros-passos-com-spring.html',
-    ],
-    [
-        'titulo' => 'O que é Metodologia Ágil?',
-        'conteudo' => 'Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
-        'link' => 'o-que-e-metodologia-agil.html',
-    ],
-    [
-        'titulo' => 'Como é o funil do Growth Hacking?',
-        'conteudo' => 'Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-        mais sobre ele.',
-        'link' => 'como-e-o-funil-do-growth-hacking.html',
-    ]
+require 'config.php'; // programa não funciona
+include 'src/Artigos.php'; // programa funciona parcial
 
-];
+$artigo = new Artigo($mysql);
+$artigos = $artigo->exibirTodos();
 
 ?>
 
@@ -38,7 +23,7 @@ $artigos = [
 
         <?php foreach ($artigos as $artigo) : ?>
             <h2>
-                <a href="<?php echo $artigo['link'] ?>">
+                <a href="artigo.php?id=<?php echo $artigo['id']; ?>">
                     <?php echo $artigo['titulo'] ?>
                 </a>
             </h2>
